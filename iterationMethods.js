c4bab0ea89c7ffab319a54110a24081b694219b1
Abdulaziz-Marafi <azizbm4798@gmail.com>
2025-02-13 10:27:13 +0300

    Solved

--- iterationMethods.js
+++ iterationMethods.js
@@ -5,17 +5,20 @@ const temperatures = [
 
 // 1) Write a `filterHighTemperatures` function that returns a new array containing temperatures greater than or equal to 25 degrees Celsius.
 function filterHighTemperatures(temps) {
-  // Your code here
+  let newTemps = temps.filter((temp) => temp >= 25);
+  return newTemps;
 }
 
 // Write a `filterLowTemperatures` function that returns a new array containing temperatures less than 20 degrees Celsius.
 function filterLowTemperatures(temps) {
-  // Your code here
+  let newTemps = temps.filter((temp) => temp < 20);
+  return newTemps;
 }
 
 // Write a `convertCelsiusToFahrenheit` function that converts the entire list of temperatures to Fahrenheit, and return it. Use the formula (Celsius * 9/5) + 32.
 function convertCelsiusToFahrenheit(temps) {
-  // Your code here
+  let newTemps = temps.map((temp) => (temp * 9) / 5 + 32);
+  return newTemps;
 }
 
 // Write a `labelTemperatures` function that returns a new array that labels each temperature as either:
@@ -23,17 +26,31 @@ function convertCelsiusToFahrenheit(temps) {
 // "Mild" for temperatures between 20 and 24,
 // "Cool" for temperatures <20
 function labelTemperatures(temps) {
-  // Your code here
+  let newTemps = temps.map((temp) => {
+    if (temp >= 25) {
+      return "Warm";
+    } else if (temp >= 20 && temp <= 24) {
+      return "Mild";
+    } else {
+      return "Cool";
+    }
+  });
+  return newTemps;
 }
 
 // Write a `getMaxTemperature` function that calculates and return the highest temperature of the month.
 function getMaxTemperature(temps) {
   // Your code here
+  let top = 0;
+  temps.forEach((element) => (top >= element ? (top = top) : (top = element)));
+  return top;
 }
 
 // Write a `getMinTemperature` function that calculates and return the lowest temperature of the month.
 function getMinTemperature(temps) {
-  // Your code here
+  let bot = 50;
+  temps.forEach((element) => (bot <= element ? (bot = bot) : (bot = element)));
+  return bot;
 }
 
 module.exports = {
